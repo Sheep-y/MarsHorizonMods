c4ef913ce164a940694494ecf575499daa2a7d40
Sheep-y <your.sheepy@gmail.com>
2022-03-01 12:43:44 +0800

    Found all mission list interception points.

--- Informed/PatcherMissionPlan.cs
+++ Informed/PatcherMissionPlan.cs
@@ -15,17 +15,30 @@ namespace ZyMod.MarsHorizon.Informed {
 
       internal void Apply () {
          TryPatch( typeof( SolarScreen ), "EnterMissionSelectState", nameof( LogEnter ) ); // Initial switch from solar system to mission list
+
+         // MissionControl.Open
+         // MissionControl.SetMission
+         // MissionSelectSidebarScreen.Setup
+         // MissionSelectSidebarScreen.SetCategoryToggle
+         // MissionSelectSidebarScreen.SetState
+
+         TryPatch( typeof( MissionSelectSidebarGroup ), "AddMission", nameof( AddMission ) ); // Add mission buttons.
+         TryPatch( typeof( MissionSelectSidebarToggle ), "DoSelectToggle", nameof( DoSelectToggle ) ); // Mission button clicked.
+
          TryPatch( typeof( MissionSelectSidebarToggle ), "OnClick", nameof( OnClick ) );
          TryPatch( typeof( MissionPlanScreen ), "SetMission", nameof( SetMission ) );
          TryPatch( typeof( MissionPlanScreen ), "SetMissionSelectState", nameof( SetMissionSelectState ) );
-         TryPatch( typeof( MissionControl ), "GetMissionPlanState", nameof( LogGetMissionPlan ) ); // 4 times initial load, 3 times per select
-         TryPatch( typeof( MissionPlanOverviewToggle ), "RefreshActionIcon", nameof( LogRefreshActionIcon ) ); // 4 times initial load, 3 times per select
+
+         //TryPatch( typeof( MissionControl ), "GetMissionPlanState", nameof( LogGetMissionPlan ) ); // 4 times initial load, 3 times per select
+         //TryPatch( typeof( MissionPlanOverviewToggle ), "RefreshActionIcon", nameof( LogRefreshActionIcon ) ); // 4 times initial load, 3 times per select
       }
 
-      private static void SetMissionSelectState () => Info( "MissionPlanScreen.SetMissionSelectState" );
-      private static void SetMission ( Mission mission ) => Info( "MissionPlanScreen.SetMission {0}", mission );
       private static void LogEnter () => Info( "SolarScreen.EnterMissionSelectState" );
+      private static void DoSelectToggle () => Info( "MissionPlanScreen.DoSelectToggle" );
+      private static void AddMission ( object __instance, Mission mission ) => Info( "{1}.AddMission {0}", mission, __instance.GetType() );
+      private static void SetMission ( object __instance, Mission mission ) => Info( "{1}.SetMission {0}", mission, __instance.GetType() );
       private static void OnClick ( Mission mission ) => Info( "MissionSelectSidebarToggle.OnClick {0}", mission );
+      private static void SetMissionSelectState () => Info( "MissionPlanScreen.SetMissionSelectState" );
       private static void LogGetMissionPlan ( Mission mission ) => Info( "MissionControl.GetMissionPlanState {0}", mission );
       private static void LogRefreshActionIcon ( Mission mission ) => Info( "MissionPlanOverviewToggle.RefreshActionIcon {0}", mission );
    }
