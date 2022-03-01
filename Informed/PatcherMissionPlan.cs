fab39fc0826c3934b206fd6e3e828d54880fa62f
Sheep-y <your.sheepy@gmail.com>
2022-03-02 00:21:29 +0800

    Can get into launch window, can get out, now need to set correct body and clear any vehicle effects.

--- Informed/PatcherMissionPlan.cs
+++ Informed/PatcherMissionPlan.cs
@@ -17,13 +17,13 @@ namespace ZyMod.MarsHorizon.Informed {
       internal void Apply () {
          TryPatch( typeof( MissionSelectSidebarScreen ), "SetState", postfix: nameof( AddLaunchWindowButton ) );
          TryPatch( typeof( MissionSelectSidebarToggle ), "SetMission", prefix: nameof( SetLaunchWindowButton ) );
-         TryPatch( typeof( MissionSelectSidebarToggle ), "OnClick", prefix: nameof( ShowLaunchWindow ) );
+         TryPatch( typeof( MissionSelectSidebarToggle ), "OnPointerClick", prefix: nameof( BlockLaunchWindowDblClick ) );
       }
 
       private const string LaunchWindowGuid = "e019269c-9b9b-4ee3-ac1c-ee5c35f0e4f6";
 
       private static void AddLaunchWindowButton ( MissionSelectSidebarScreen __instance, SimplePooler<MissionSelectSidebarGroup> ___missionGroupPooler ) { try {
-         Fine( "Adding the launch window button in mission select screen." );
+         Info( "Adding launch window button to {0} mission list.", MissionControl.PlanetaryBody );
          var group = ___missionGroupPooler.Get();
          group.Setup( "Title_Calendar" );
          var mission = new Mission( __instance.agency.missions.First() ){ guid = LaunchWindowGuid };
@@ -39,17 +39,23 @@ namespace ZyMod.MarsHorizon.Informed {
          ___missionIcon.gameObject.SetActive( false );
          ___toggle.onValueChanged.RemoveAllListeners();
          ___toggle.isOn = false;
-         ___toggle.onValueChanged.AddListener( ( isOn ) => {
+         ___toggle.onValueChanged.AddListener( ( isOn ) => { try {
             if ( ! isOn ) return;
-            Fine( "Launch Window Button clicked for {0}", MissionControl.PlanetaryBody );
-         } );
+            var body = MissionControl.PlanetaryBody;
+            Fine( "Launch Window Button clicked for {0}", body );
+            var controller = Controller.Instance;
+            void Back () {
+               Fine( "Moving back!" );
+               controller.gameUI.SetViewState( controller.clientViewer.stateMissionControlMissionSelect, true );
+            }
+            controller.clientViewer.EnterCalendarScheduleState( mission, Back, Back );
+         } catch ( Exception x ) { Err( x ); } } );
          type.Property( "Mission" ).SetValue( __instance, mission );
          type.Method( "SetMissionState" ).Run( __instance, mission, MissionSelectSidebarToggle.EState.None );
-         Fine( "Configured launch window sidebar button" );
          return false;
       } catch ( Exception x ) { return Err( x, false ); } }
 
-      private static bool ShowLaunchWindow ( Mission mission ) => mission.guid != LaunchWindowGuid;
+      private static bool BlockLaunchWindowDblClick ( MissionSelectSidebarToggle __instance ) => __instance.Mission.guid != LaunchWindowGuid;
    }
 
    //internal class LaunchWindowMission : Mission {}
