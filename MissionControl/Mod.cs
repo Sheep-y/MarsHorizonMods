a4f68fd30bbe13d4a7fabc6b2a1b59e3e4f8e733
Sheep-y <your.sheepy@gmail.com>
2022-03-05 00:39:46 +0800

    Implement mission chance, planet weight, and variation weights changes.

--- MissionControl/Mod.cs
+++ MissionControl/Mod.cs
@@ -4,6 +4,7 @@ using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
@@ -37,52 +38,122 @@ namespace ZyMod.MarsHorizon.MissionControl {
    }
 
    public class Config : IniConfig {
-      [ Config( "Weight of Jupiter mission.  Set to 0 to disable." ) ]
-      public int jupiter_mission_weight = 1;
-      [ Config( "Weight of Saturn mission.  Set to 0 to disable." ) ]
-      public int saturn_mission_weight = 1;
-      [ Config( "Weight of Uranus mission.  Set to 0 to disable." ) ]
-      public int uranus_mission_weight = 1;
-      [ Config( "Weight of Neptune mission.  Set to 0 to disable." ) ]
-      public int neptune_mission_weight = 1;
-      [ Config( "Weight of Pluto mission.  Set to 0 to disable." ) ]
-      public int pluto_mission_weight = 1;
+      [ Config( "Chance of new request mission for player.  Game default 0.25 (for 25%).  Set to -1 to not change (default)." ) ]
+      public float player_request_mission_chance = -1;
+      [ Config( "Chance of new request mission for AI.  Ditto." ) ]
+      public float ai_request_mission_chance = -1;
+      [ Config( "Limit weight changes to player agency only.  Default true." ) ]
+      public bool reweight_only_player_agency = true;
+
+      [ Config( "[Variations]\r\n; Multiply lucartive variation chances.  Default 2.4.  Set to 1 to not change." ) ]
+      public float lucrative_weight_multiplier = 2.4f;
+      [ Config( "Try divide all variation weight by this amount to reduce work.  Affects lucrative_weight_multiplier accuracy.  Default 10.  Set to 1 to not change." ) ]
+      public int variation_weight_divider = 10;
+
+      [ Config( "[Earth and Moon]\r\n; Weight of each uncrewed Earth mission.  Set to 0 to eliminate, -1 to not change (100).  Same for all below" ) ]
+      public int earth_uncrewed_mission_weight = 40;
+      public int earth_crewed_mission_weight = 20;
+      public int moon_uncrewed_mission_weight = 20;
+      public int moon_crewed_mission_weight = 10;
+      public int space_station_mission_weight = 10;
+
+      [ Config( "[Inner Planets]" ) ]
+      public int venus_mission_weight = 3;
+      public int mercury_mission_weight = 2;
+
+      [ Config( "[Outter Planets]" ) ]
+      public int mars_mission_weight = 5;
+      public int jupiter_mission_weight = 0;
+      public int saturn_mission_weight = 0;
+      public int uranus_mission_weight = 0;
+      public int neptune_mission_weight = 0;
+      public int pluto_mission_weight = 0;
+
+      [ Config( "[Others]" ) ]
+      public int other_mission_weight = 0;
+
    }
 
    internal class PatcherSimulation : ModPatcher {
       internal void Apply () {
-         TryPatch( typeof( Simulation ).Method( "AgencyTryGenerateRequestMissionType" ), transpiler: nameof( InterceptMissionTypes ) );
+         TryPatch( typeof( Simulation ).Method( "AgencyTryGenerateMissionRequestMessage" ), prefix: nameof( TrackNewMissionBefore ), postfix: nameof( TrackNewMissionAfter ) );
+         TryPatch( typeof( Simulation ).Method( "AgencyTryGenerateRequestMissionType" ), prefix: nameof( SetMissionWeight ) );
       }
 
-      private static IEnumerable< CodeInstruction > InterceptMissionTypes ( IEnumerable< CodeInstruction > codes ) {
-         var list = codes.ToList();
-         if ( list.Count <= 20 ) { Error( "Method too short. Aborting."); return codes; }
-         var pos = list.Count - 20;
-         CodeInstruction label = list[ pos ], ldloc = list[ pos + 1 ], count = list[ pos + 2 ];
-         var cTxt = count.ToString();
-         if ( label.opcode == OpCodes.Blt &&
-              ldloc.opcode == OpCodes.Ldloc_1 &&
-              count.opcode == OpCodes.Callvirt && cTxt.Contains( ".RequestMissionType>" ) && cTxt.EndsWith( "Count()" ) ) {
-            Info( "CIL found, intercepting mission type list" );
-            list.Insert( pos + 1, new CodeInstruction( OpCodes.Ldloc_1 ) );
-            list.Insert( pos + 2, new CodeInstruction( OpCodes.Call, typeof( PatcherSimulation ).Method( "ProcessMissionTemplate" ) ) );
-         } else {
-            Error( "CIL mismatch.  Patch failure. {0} {1} {2}", label, ldloc, count );
-            if ( RootMod.Log.LogLevel == System.Diagnostics.TraceLevel.Verbose )
-               foreach ( var code in list )
-                  Fine( code );
+      private static float origChance = -1f;
+      private static readonly Dictionary< Data.MissionTemplate, int > origWeight = new Dictionary< Data.MissionTemplate, int >();
+
+      private static void TrackNewMissionBefore ( Simulation __instance, Agency agency, bool forceGenerate ) { try {
+         origWeight.Clear();
+         if ( forceGenerate ) {
+            Info( "{0} is forcing a new mission.", agency.NameLocalised );
+            return;
          }
-         return list;
+         var chance = agency.isAI ? config.ai_request_mission_chance : config.player_request_mission_chance;
+         var rules = __instance.gamedata.rules;
+         if ( origChance < 0 ) origChance = rules.requestGenerationChance;
+         if ( chance < 0 || chance > 1 ) chance = origChance;
+         rules.requestGenerationChance = chance;
+         RootMod.Log?.Write( agency.isAI ? TraceLevel.Verbose : TraceLevel.Info,
+            "{0} checking new mission.  Current count {2}/{3}, cooldown {4}, chance {1}.", agency.NameLocalised,
+            rules.requestGenerationChance, agency.RequestMissionCount, __instance.gamedata.GetEraRequestLimit( agency.era ), agency.turnsUntilNextMissionRequest );
+      } catch ( Exception x ) { Err( x ); } }
+
+      private static void TrackNewMissionAfter ( Agency agency, NetMessages.MissionRequest message, bool __result ) { try {
+         if ( ! canMod( agency ) ) return;
+         if ( origWeight.Count > 0 ) {
+            Fine( "Restoring {0} weights.", origWeight.Count );
+            foreach ( var pair in origWeight ) pair.Key.requestWeighting = pair.Value;
+            origWeight.Clear();
+         }
+         if ( ! __result ) { Fine( "No new mission." ); return; }
+         Info( "New mission {0} {1} {2}.  Next mission after {3}+ turns.", message.milestoneMissionID, message.requestMissionType, message.isJointMission ? "(Join)" : "", message.turnsUntilNextMissionRequest );
+      } catch ( Exception x ) { Err( x ); } }
+
+      private static void SetMissionWeight ( Agency agency, Data.MissionTemplate missionTemplate ) { try {
+         if ( !canMod( agency ) ) return;
+         var m = missionTemplate;
+         var weight = GetMissionWeight( m );
+         if ( weight < 0 ) weight = m.requestWeighting;
+         Fine( "Mission {0}, weight {1}{2}", m.primaryMilestone, m.requestWeighting, weight == m.requestWeighting ? "" : $" => {weight}" );
+         if ( weight != m.requestWeighting ) {
+            origWeight[ m ] = m.requestWeighting;
+            m.requestWeighting = weight;
+         }
+         SetVariationWeights( m );
+      } catch ( Exception x ) { Err( x ); } }
+
+      private static int GetMissionWeight ( Data.MissionTemplate m ) {
+         switch ( m.planetaryBody ) {
+            case Data.PlanetaryBody.Earth:
+               if ( m.crewRemainOnStation ) return config.space_station_mission_weight;
+               return m.IsCrewed ? config.earth_crewed_mission_weight : config.earth_uncrewed_mission_weight;
+            case Data.PlanetaryBody.Moon:
+               return m.IsCrewed ? config.moon_crewed_mission_weight : config.moon_uncrewed_mission_weight;
+            case Data.PlanetaryBody.Mercury: return config.mercury_mission_weight;
+            case Data.PlanetaryBody.Venus: return config.venus_mission_weight;
+            case Data.PlanetaryBody.Mars: return config.mars_mission_weight;
+            case Data.PlanetaryBody.Jupiter: return config.jupiter_mission_weight;
+            case Data.PlanetaryBody.Saturn: return config.saturn_mission_weight;
+            case Data.PlanetaryBody.Uranus: return config.uranus_mission_weight;
+            case Data.PlanetaryBody.Neptune: return config.neptune_mission_weight;
+            case Data.PlanetaryBody.Pluto: return config.pluto_mission_weight;
+            default: return config.other_mission_weight;
+         }
       }
 
-      public static void ProcessMissionTemplate ( List< Data.MissionTemplate.RequestMissionType > list ) {
-         Info( list.Count );
-         foreach ( var m in list ) {
-            Info( "{3} {0} Era[{1}] Tech[{2}]", m.type,
-               string.Join( ",", m.eraDependencies.Select( e => e + "" ) ),
-               string.Join( ",", m.researchDependencies.Select( e => e + "" ) ),
-               m.weighting );
+      private static void SetVariationWeights ( Data.MissionTemplate m ) {
+         var divider = config.variation_weight_divider;
+         if ( divider != 1f  ) divider = m.requestMissionTypes.All( e => e.weighting % divider == 0 ) ? divider : 1;
+         foreach ( var t in m.requestMissionTypes ) {
+            var tWeight = t.weighting;
+            if ( t.type == Data.MissionTemplate.RequestMissionType.EType.Lucrative ) tWeight = (int) Math.Round( tWeight * config.lucrative_weight_multiplier );
+            if ( divider != 1 ) tWeight /= divider;
+            Fine( "   > Variation {0}, weight {1}{2}", t.type, t.weighting, t.weighting == tWeight ? "" : $" => {tWeight}" );
+            t.weighting = tWeight;
          }
       }
+
+      private static bool canMod ( Agency agency ) => ! config.reweight_only_player_agency || ! agency.isAI;
    }
 }
