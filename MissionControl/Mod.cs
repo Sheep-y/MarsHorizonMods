cd6374d74ad49282cb22d926083bbf2203a322be
Sheep-y <your.sheepy@gmail.com>
2022-03-05 01:24:43 +0800

    Fix config, avoid repeated log, and track mission removal.

--- MissionControl/Mod.cs
+++ MissionControl/Mod.cs
@@ -51,40 +51,47 @@ namespace ZyMod.MarsHorizon.MissionControl {
       public int variation_weight_divider = 10;
 
       [ Config( "\r\n[Earth and Moon]\r\n; Weight of each uncrewed Earth mission.  Set to 0 to eliminate, -1 to not change (100).  Same for all below" ) ]
-      public int earth_uncrewed_mission_weight = 30;
-      public int earth_crewed_mission_weight = 20;
-      public int moon_uncrewed_mission_weight = 20;
-      public int moon_crewed_mission_weight = 10;
-      public int space_station_mission_weight = 10;
+                   public int earth_uncrewed_mission_weight = 30;
+      [ Config() ] public int earth_crewed_mission_weight = 20;
+      [ Config() ] public int moon_uncrewed_mission_weight = 20;
+      [ Config() ] public int moon_crewed_mission_weight = 10;
+      [ Config() ] public int space_station_mission_weight = 10;
 
       [ Config( "\r\n[Inner Planets]" ) ]
-      public int venus_mission_weight = 3;
-      public int mercury_mission_weight = 2;
+                   public int venus_mission_weight = 3;
+      [ Config() ] public int mercury_mission_weight = 2;
 
       [ Config( "\r\n[Outter Planets]" ) ]
-      public int mars_mission_weight = 5;
-      public int jupiter_mission_weight = 0;
-      public int saturn_mission_weight = 0;
-      public int uranus_mission_weight = 0;
-      public int neptune_mission_weight = 0;
-      public int pluto_mission_weight = 0;
+                   public int mars_mission_weight = 5;
+      [ Config() ] public int jupiter_mission_weight = 0;
+      [ Config() ] public int saturn_mission_weight = 0;
+      [ Config() ] public int uranus_mission_weight = 0;
+      [ Config() ] public int neptune_mission_weight = 0;
+      [ Config() ] public int pluto_mission_weight = 0;
 
       [ Config( "\r\n[Others]" ) ]
       public int other_mission_weight = 0;
 
+      [ Config( "\r\n; Version of this mod config file.  Do not change." ) ]
+      public int config_version = 20200304;
    }
 
    internal class PatcherSimulation : ModPatcher {
       internal void Apply () {
          TryPatch( typeof( Simulation ).Method( "AgencyTryGenerateMissionRequestMessage" ), prefix: nameof( TrackNewMissionBefore ), postfix: nameof( TrackNewMissionAfter ) );
-         TryPatch( typeof( Simulation ).Method( "AgencyTryGenerateRequestMissionType" ), prefix: nameof( SetMissionWeight ) );
+         TryPatch( typeof( Simulation ).Method( "AgencyTryGenerateRequestMissionType" ), prefix: nameof( SetMissionWeight ), postfix: nameof( LogMissionRemoval ) );
+         TryPatch( typeof( Simulation ).Method( "AgencyTryGenerateRequestMissionContext" ), postfix: nameof( LogMissionRemoval ) );
       }
 
       private static float origChance = -1f;
       private static readonly Dictionary< Data.MissionTemplate, int > origWeight = new Dictionary< Data.MissionTemplate, int >();
 
+      private static Data.MissionTemplate lastMission = null;
+      private static bool allowed = false;
+
       private static void TrackNewMissionBefore ( Simulation __instance, Agency agency, bool forceGenerate ) { try {
          origWeight.Clear();
+         lastMission = null;
          if ( forceGenerate ) {
             Info( "{0} is forcing a new mission.", agency.NameLocalised );
             return;
@@ -101,6 +108,7 @@ namespace ZyMod.MarsHorizon.MissionControl {
 
       private static void TrackNewMissionAfter ( Agency agency, NetMessages.MissionRequest message, bool __result ) { try {
          if ( ! canMod( agency ) ) return;
+         lastMission = null;
          if ( origWeight.Count > 0 ) {
             Fine( "Restoring {0} weights.", origWeight.Count );
             foreach ( var pair in origWeight ) pair.Key.requestWeighting = pair.Value;
@@ -111,8 +119,9 @@ namespace ZyMod.MarsHorizon.MissionControl {
       } catch ( Exception x ) { Err( x ); } }
 
       private static void SetMissionWeight ( Agency agency, Data.MissionTemplate missionTemplate ) { try {
-         if ( !canMod( agency ) ) return;
-         var m = missionTemplate;
+         if ( ! canMod( agency ) || missionTemplate == lastMission ) return;
+         var m = lastMission = missionTemplate;
+         allowed = true;
          var weight = GetMissionWeight( m );
          if ( weight < 0 ) weight = m.requestWeighting;
          Fine( "Mission {0}, weight {1}{2}", m.primaryMilestone, m.requestWeighting, weight == m.requestWeighting ? "" : $" => {weight}" );
@@ -152,6 +161,12 @@ namespace ZyMod.MarsHorizon.MissionControl {
             Fine( "   > Variation {0}, weight {1}{2}", t.type, t.weighting, t.weighting == tWeight ? "" : $" => {tWeight}" );
             t.weighting = tWeight;
          }
+      }
+
+      private static void LogMissionRemoval ( bool __result ) {
+         if ( __result == allowed ) return;
+         Fine( allowed ? "X Mission removed due to era, research, or ongoing mission." : "X ... then restored after forced generation." );
+         allowed = __result;
       }
 
       private static bool canMod ( Agency agency ) => ! config.reweight_only_player_agency || ! agency.isAI;
