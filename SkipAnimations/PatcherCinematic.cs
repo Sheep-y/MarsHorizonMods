265e2f7a723023673dffa003333871f0d403b1fa
Sheep-y <your.sheepy@gmail.com>
2022-03-06 15:20:09 +0800

    Log cinematic info to identify "Can't find valid cinematic setup" error.

--- SkipAnimations/PatcherCinematic.cs
+++ SkipAnimations/PatcherCinematic.cs
@@ -1,4 +1,5 @@
 ﻿using Astronautica.View;
+using Astronautica.View.Cinematics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,8 @@ namespace ZyMod.MarsHorizon.SkipAnimations {
             TryPatch( typeof( SplashDelayScene ), "Start", nameof( SkipSplash ) );
          if ( config.skip_all_cinematic || config.skip_seen_cinematic || config.skip_seen_cinematic_until_exit || config.SkipCinematics.Count > 0 )
             TryPatch( typeof( CinematicSceneController ), "GetInputDownSkip", postfix: nameof( SkipCinmatic ) );
+         if ( RootMod.Log.LogLevel == System.Diagnostics.TraceLevel.Verbose )
+            TryPatch( typeof( CinematicSetupCreator ).Method( "TryGetValidSetup", typeof( CinematicSetup.ECategory ), typeof( CinematicData ), typeof( CinematicSetup ).MakeByRefType() ), postfix: nameof( LogCinematic ) );
       }
 
       private static void SkipSplash () {
@@ -66,5 +69,16 @@ namespace ZyMod.MarsHorizon.SkipAnimations {
          __result = ShouldSkip( id );
       } catch ( Exception x ) { Err( x ); } }
 
+      // Trying to tackle the "Can't find valid cinematic setup" error.
+      private static void LogCinematic ( CinematicSetup.ECategory category, CinematicData cinematicData, CinematicSetup cinematicSetup, bool __result ) { try {
+         var c = cinematicData;
+         Fine( "{4} cinematic {2}:{3} for {0} {1}", category, c, cinematicSetup?.name, cinematicSetup?.SceneName, __result ? "Found" : "Not Found" );
+         Fine( "Agency {0}, cId {1}, planet {2}, launchpad {3}, launch result {4}, mission {5}, mission state {6}, model {7}" +
+               ", next planet {8}, payload {9}, phase {10}, task {11}, isShuttle {12}, isReusable {13}" +
+               ", mission context {14}, crew {15}"
+            , c.agency?.type, c.cinematicAssetId, c.currentPlanetaryBody, c.launchRequirement, c.launchState, c.mission?.template?.id, c.missionState, c.modelName
+            , c.nextPlanetaryBody, c.payload?.id, c.phaseIndex, c.taskIndex, c.vehicle?.isShuttle, c.vehicle?.isReusable
+            , c.mission.requestMissionContext?.localisationReference, c.mission?.template?.minCrew );
+      } catch ( Exception x ) { Err( x ); } }
    }
 }
