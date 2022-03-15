5066dbcbc16636a1e75360f6b9629b85399445be
Sheep-y <your.sheepy@gmail.com>
2022-03-15 14:41:56 +0800

    Fix auto resolve rng not working.

--- MissionControl/PatcherAutoResolve.cs
+++ MissionControl/PatcherAutoResolve.cs
@@ -7,15 +7,19 @@ namespace ZyMod.MarsHorizon.MissionControl {
 
    internal class PatcherAutoResolve: ModPatcher {
       internal void Apply () {
-         TryPatch( typeof( AutoresolveMission ).Method( "CalculateSuccess" ), prefix: nameof( StandaloneAutoResolve ) );
+         TryPatch( typeof( AutoresolveMission ).Method( "CalculateSuccess" ), prefix: nameof( StandaloneAutoResolve ), postfix: nameof( LogAutoResolve ) );
       }
 
       private static readonly Random resolveRng = new Random();
-      private static readonly FieldInfo ResolveRoll = typeof( AutoresolveMission ).Field( "Roll" );
+      private static readonly PropertyInfo ResolveRoll = typeof( AutoresolveMission ).Property( "Roll" );
+      private static float oldRoll;
 
       private static void StandaloneAutoResolve ( AutoresolveMission __instance ) { try {
-         var oldRoll = __instance.Roll;
-         ResolveRoll.SetValue( __instance, (float) resolveRng.NextDouble() );
+         oldRoll = __instance.Roll;
+         ResolveRoll?.SetValue( __instance, (float) resolveRng.NextDouble() );
+      } catch ( Exception x ) { Err( x ); } }
+
+      private static void LogAutoResolve ( AutoresolveMission __instance ) { try {
          Info( "Auto-resolve mission roll: {0:P2} => {1:P2}, Fail {2}%, Perfect {3}%", oldRoll, __instance.Roll, __instance.FailureChance, __instance.OutstandingChance );
       } catch ( Exception x ) { Err( x ); } }
    }
