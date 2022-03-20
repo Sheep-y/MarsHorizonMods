ba94bed0fc5d87a57003bd8a473da2448fe88eb0
Sheep-y <your.sheepy@gmail.com>
2022-03-21 01:03:23 +0800

    Fix traditional chinese display by adding to fallback list.

--- Zhant/Mod.cs
+++ Zhant/Mod.cs
@@ -7,6 +7,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using static ZyMod.ModHelpers;
 
 namespace ZyMod.MarsHorizon.Zhant {
@@ -14,34 +18,53 @@ namespace ZyMod.MarsHorizon.Zhant {
    public class Mod : MarsHorizonMod {
       protected override string GetModName () => "Zhant";
       protected override void OnGameAssemblyLoaded ( Assembly game ) {
-         new PatcherL10N().Apply();
+         new PatcherL10N().Apply( game );
       }
    }
 
    internal class PatcherL10N : Patcher {
       private static PatcherL10N me;
 
-      internal void Apply () {
+      internal void Apply ( Assembly game ) {
          me = this;
          TryPatch( typeof( Controller ), "OnSystemLanguageChanged", postfix: nameof( DynamicPatch ) );
          TryPatch( typeof( UserSettings ), "SetLanguage", postfix: nameof( DynamicPatch ) );
       }
 
-      private static ModPatch patch;
+      private static ModPatch patchZh, patchFont;
 
-      private static void DynamicPatch () { lock ( me ) {
+      private static void DynamicPatch () { lock ( me ) { try {
          var locale = ScriptableObjectSingleton<Localisation>.instance.locale;
          Info( "Locale is {0}", locale );
          if ( locale == "zh_cn" ) {
-            if ( patch == null )
-               patch = me.TryPatch( typeof( Localisation ).Method( "Interpolate", typeof( string ), typeof( Dictionary< string, string > ) ), postfix: nameof( ToZht ) );
-         } else if ( patch != null ) {
-            patch.Unpatch();
-            patch = null;
+            if ( patchZh == null ) {
+               patchZh = me.TryPatch( typeof( Localisation ).Method( "Interpolate", typeof( string ), typeof( Dictionary<string, string> ) ), prefix: nameof( ToZht ) );
+            }
+            LoadFonts();
+         } else if ( patchZh != null ) {
+            patchZh?.Unpatch();
+            patchFont?.Unpatch();
+            patchZh = patchFont = null;
          }
-      } }
+      } catch ( Exception x ) { Err( x ); } } }
 
-      private static readonly Dictionary< string, string > zhs2zht = new Dictionary<string, string>();
+      private static void LoadFonts () {
+         if ( zhs2zht.Count != 0 || zhtFonts.Count != 0 ) return;
+         foreach ( var v in new string[] { "Regular", "Medium", "Bold" } ) try {
+            var f = Path.Combine( ModDir, $"NotoSansHK-{v}.otf" );
+            if ( File.Exists( f ) ) {
+               Info( "Loading {0}", f );
+               zhtFonts[ v ] = new Font( f );
+               zhtTMPFs[ v ] = TMP_FontAsset.CreateFontAsset( zhtFonts[ v ] );
+            } else
+               Warn( "Font {0} not found.", f );
+         } catch ( Exception x ) { Err( x ); }
+         TMP_Settings.fallbackFontAssets.Add( zhtTMPFs[ "Medium" ] );
+      }
+
+      private static readonly Dictionary< string, string > zhs2zht = new Dictionary< string, string >();
+      private static readonly Dictionary< string, Font > zhtFonts = new Dictionary< string, Font >();
+      private static readonly Dictionary< string, TMP_FontAsset > zhtTMPFs = new Dictionary< string, TMP_FontAsset >();
       
       private static void ToZht ( ref string text ) { try {
          if ( string.IsNullOrEmpty( text ) ) return;
@@ -55,15 +78,23 @@ namespace ZyMod.MarsHorizon.Zhant {
          Fine( "{0} => {1} ({2} chars)", text, zht, zht.Length );
          zhs2zht.Add( text, text = zht );
       } catch ( Exception x ) { Err( x ); } }
+
       private static readonly string[] tweaks = new string[]{
          "游戲", "遊戲",
 
+         "最后", "最後",
+         "并", "並",
+         "进", "進",
          "于", "於",
          "剩余", "剩餘",
          "加載遊戲", "載入遊戲",
          "有效載荷", "酬載",
+         "正在登錄", "載入",
          "菜單", "選單",
-         "采集", "采集",
+         "采集", "採集",
+         "采樣", "採樣",
+         "阿麗亞娜", "亞利安",
+         "大力神", "泰坦",
       };
 
       private static string ZhtTweaks ( string txt ) {
