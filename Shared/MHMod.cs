711ddd639c7836c3f176ebe92ba8fe01e1929ea9
Sheep-y <your.sheepy@gmail.com>
2022-03-30 17:35:53 +0800

    Validate loaded config on load/change/reload, save changes back to BepInEx.

--- Shared/MHMod.cs
+++ Shared/MHMod.cs
@@ -1,13 +1,16 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
+using HarmonyLib;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 using UnityModManagerNet;
 
 // Common base mod for Mars Horizon.  Find and initialize the correct class, set log and config path and name.
@@ -35,7 +38,7 @@ namespace ZyMod.MarsHorizon {
 
       internal static void Apply () { try {
          var mod = instance as MarsHorizonMod;
-         mod?.OnGameAssemblyLoaded( null );
+         lock ( sync ) mod?.OnGameAssemblyLoaded( null );
          mod?.CountPatches();
       } catch ( Exception x ) { Err( x ); } }
 
@@ -80,81 +83,120 @@ namespace ZyMod.MarsHorizon {
 
    internal class BepInUtil : ModComponent {
       internal static void Setup ( BaseUnityPlugin mod, BaseConfig config = null ) {
-         lock( sync ) ModPath = mod.Info.Location;
+         lock( sync ) { BepInUtil.mod = mod; ModPath = mod.Info.Location; } // ModPath is null when loaded by ScriptEngine
          SetLogger( typeof( BaseUnityPlugin ).Property( "Logger" ).GetValue( mod ) as ManualLogSource );
          BindConfig( mod.Config, config );
       }
 
       internal static void Unbind () {
          MarsHorizonMod.Unload();
-         configBindings.Clear();
+         bindings?.Clear();
+         mod = null;
       }
 
+      private static BaseUnityPlugin mod;
       private static BaseConfig modConfig;
-      private static Dictionary< string, object > configBindings;
+      private static Dictionary< FieldInfo, object > bindings;
 
-      internal static void BindConfig ( ConfigFile Config, BaseConfig conf ) { try {
+      internal static void BindConfig ( ConfigFile Config, BaseConfig conf ) { lock ( sync ) try {
          if ( Config == null || conf == null ) return;
          modConfig = conf;
-         var type = conf.GetType();
+         Type type = conf.GetType(), strT = typeof( string );
          var fields = typeof( BaseConfig ).Method( "_ListFields" ).Run( conf, conf ) as IEnumerable< FieldInfo >;
          var bind = typeof( ConfigFile ).Methods( "Bind" ).First( e =>
             e.GetParameters().Length == 4 && e.GetParameters()[ 3 ].ParameterType == typeof( string ) );
          var section = "";
-         configBindings?.Clear();
-         Info( "Loading config from BepInEx." );
-         foreach ( var f in fields ) {
-            if ( f.Name == "config_version" ) continue;
-            BindConfigField( Config, f, bind, ref section );
-         }
-         if ( configBindings?.Count > 0 )
-            Config.ConfigReloaded += GetOnReloadListener();
+         bindings?.Clear();
+         Info( "Loading config through BepInEx from {0}.", Config.ConfigFilePath );
+         foreach ( var f in fields ) BindConfigField( Config, f, bind, ref section );
+         new Harmony( ModName + ".Bep" ).Patch( type.Method( "OnSaving", strT ) ?? typeof( BaseConfig ).Method( "OnSaving", strT ),
+            prefix: new HarmonyMethod( typeof( BepInUtil ).Method( nameof( SaveToBep ) ) ) );
+         ValidateConfig();
+         if ( bindings?.Count > 0 ) Config.ConfigReloaded += GetOnReloadListener();
          MarsHorizonMod.configLoaded = true;
       } catch ( Exception x ) { Err( x ); } }
 
-      private static string BindConfigField ( ConfigFile Config, FieldInfo f, MethodInfo bind, ref string section ) {
+      private static void BindConfigField ( ConfigFile Config, FieldInfo f, MethodInfo bind, ref string section ) {
+         if ( f.Name == "config_version" ) return;
          var tags = f.GetCustomAttributes( true ).OfType<ConfigAttribute>();
          ConfigAttribute sec = tags.FirstOrDefault( e => e.Comment?.EndsWith( "]" ) == true ), desc = tags.LastOrDefault();
          if ( sec?.Comment.Contains( '[' ) == true ) section = sec.Comment.Split( '[' )[ 1 ].Trim( ']' );
          var defVal = f.GetValue( modConfig );
 
          var b = bind.MakeGenericMethod( f.FieldType ).Run( Config, section, f.Name, defVal, desc?.Comment ?? "" );
-         if ( TryGetBoxedValue( b, out var val ) ) {
-            if ( !Equals( val, defVal ) ) f.SetValue( modConfig, val );
-            Fine( "Config {0} = {1}", f.Name, val );
-         } else Warn( "Cannot read {0}", f.Name );
+         if ( ! TryGetValues( null, b, out var val, out _ ) ) return;
+         if ( ! Equals( val, defVal ) ) f.SetValue( modConfig, val );
+         Fine( "Config {0} = {1}", f.Name, val );
 
          b.GetType().GetEvent( "SettingChanged" )?.AddEventHandler( b, GetOnChangeListener( f ) );
-         if ( configBindings == null ) configBindings = new Dictionary<string, object>();
-         configBindings.Add( f.Name, b );
-         return section;
+         if ( bindings == null ) bindings = new Dictionary< FieldInfo, object >();
+         bindings.Add( f, b );
       }
 
-      private static bool TryGetBoxedValue ( object binding, out object value ) {
+      private static bool TryGetValues ( FieldInfo confField, object binding, out object bVal, out object myVal ) {
          var box = binding.GetType().Property( "BoxedValue" );
-         if ( box == null ) { value = null; return false; }
-         value = box.GetValue( binding );
+         if ( box == null ) { Warn( "Cannot get BoxedValue from {0}.", binding ); bVal = myVal = null; return false; }
+         bVal = box.GetValue( binding );
+         myVal = confField?.GetValue( modConfig );
          return true;
       }
 
       internal static EventHandler GetOnChangeListener ( FieldInfo field ) { return ( _, evt ) => {
          if ( ! ( evt is SettingChangedEventArgs e ) || e.ChangedSetting == null ) return;
-         var val = e.ChangedSetting.BoxedValue;
-         Info( "Config {0} changed to {1}.", field.Name, val );
-         field.SetValue( modConfig, val );
-         MarsHorizonMod.ReApply();
+         lock ( sync ) {
+            object bVal = e.ChangedSetting.BoxedValue, myVal = field.GetValue( modConfig );
+            if ( Equals( bVal, myVal ) ) return;
+            Info( "Config {0} changed to {1} by BepInEx.", field.Name, bVal );
+            field.SetValue( modConfig, bVal );
+         }
+         ScheduleReapply();
       }; }
 
       internal static EventHandler GetOnReloadListener () { return ( _, evt ) => {
-         var type = modConfig.GetType();
-         Info( "Config reloaded." );
-         foreach ( var b in configBindings ) {
-            if ( ! TryGetBoxedValue( b, out var val ) ) continue;
-            Fine( "Config {0} = {1}", b.Key, val );
-            type.Field( b.Key ).SetValue( modConfig, val );
+         Info( "Config reloaded by BepInEx." );
+         lock ( sync ) foreach ( var b in bindings ) {
+            if ( ! TryGetValues( b.Key, b.Value, out var bVal, out var myVal ) || Equals( bVal, myVal ) ) continue;
+            Fine( "Config {0} = {1}", b.Key.Name, bVal );
+            b.Key.SetValue( modConfig, bVal );
          }
-         MarsHorizonMod.ReApply();
+         ScheduleReapply();
       }; }
+
+      private static Task ReapplyMod;
+
+      private static void ScheduleReapply () { lock ( sync ) {
+         if ( ReapplyMod != null || mod == null ) return;
+         ReapplyMod = Task.Run( async () => {
+            await Task.Delay( 50 );
+            mod.StartCoroutine( ReapplyConfig() );
+         } );
+      } }
+
+      private static IEnumerator ReapplyConfig () {
+         lock ( sync ) ReapplyMod = null;
+         ValidateConfig();
+         MarsHorizonMod.ReApply();
+         yield break;
+      }
+
+      private static void ValidateConfig () {
+         modConfig.GetType().Method( "OnLoad", typeof( string ) )?.Run( modConfig, mod?.Config.ConfigFilePath );
+         SyncToBep();
+      }
+
+      private static void SyncToBep () {
+         foreach ( var b in bindings ) {
+            if ( ! TryGetValues( b.Key, b.Value, out var bVal, out var myVal ) || Equals( bVal, myVal ) ) continue;
+            Fine( "Sync Config {0} ({1}) to BepInEx ({2}).", b.Key.Name, myVal, bVal );
+            b.Value.GetType().Property( "BoxedValue" ).SetValue( b.Value, myVal );
+         }
+      }
+ 
+      private static bool SaveToBep ( ref bool __result ) { try {
+         __result = false;
+         lock ( sync ) SyncToBep();
+         return false;
+      } catch ( Exception x ) { return Err( x, false ); } }
 
       internal static void SetLogger ( ManualLogSource logger ) {
          if ( logger == null ) return;
